f3e71780577676e674b938fea47bff667b2e1560
Angel_zapata <22170124@uttcampus.edu.mx>
2023-06-28 21:42:47 -0600

    app vue y disque inicio de secion

---
+++ src/views/iniciar_secion.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="container" >
+      <h1>Iniciar Sesión</h1>
+      <form @submit.prevent="login">
+        <label for="username">Nombre de usuario:</label>
+        <input type="text" id="username" v-model="username">
+        <label for="password">Contraseña:</label>
+        <input type="password" id="password" v-model="password">
+        <button type="submit">Iniciar Sesión</button>
+      </form>
+      <div v-if="error" class="error">{{ error }}</div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        username: '',
+        password: '',
+        error: ''
+      };
+    },
+    methods: {
+      login() {
+        // Realizar la lógica de inicio de sesión aquí, como enviar una solicitud a un servidor API
+        
+        // Ejemplo de verificación básica
+        if (this.username === 'admin' && this.password === 'password') {
+          // Inicio de sesión exitoso
+          this.error = '';
+          alert('Inicio de sesión exitoso');
+          // Aquí puedes redirigir al usuario a otra página después del inicio de sesión
+        } else {
+          // Credenciales incorrectas
+          this.error = 'Credenciales incorrectas. Inténtalo de nuevo.';
+        }
+      }
+    }
+  };
+  </script>
+  
+  <style>
+  .error {
+    color: red;
+    margin-top: 10px;
+  }
+  .container{
+    display: flex-start;
+    align-content:center;
+    
+  }
+  </style>
+  
