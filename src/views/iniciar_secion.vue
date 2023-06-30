c14aecf43089f76a0eee6bab9bfecf06deb81bb1
Angel_zapata <22170124@uttcampus.edu.mx>
2023-06-29 18:06:49 -0600

    trate de darle algo de forma al login le dare mas diseño zapata

--- src/views/iniciar_secion.vue
+++ src/views/iniciar_secion.vue
@@ -41,14 +41,40 @@
   </script>
   
   <style>
-  .error {
-    color: red;
-    margin-top: 10px;
-  }
-  .container{
-    display: flex-start;
-    align-content:center;
-    
-  }
+ .container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+.login-form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 300px;
+  margin-bottom: 20px;
+}
+
+input[type="text"],
+input[type="password"] {
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+
+button[type="submit"] {
+  padding: 10px 20px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+}
+
+.error {
+  color: red;
+  margin-top: 10px;
+}
   </style>
   
